960a61ef5603ee2aa74904ff8dc1184839b92bba
jichuyaaaa <lalapaaaa22@gmail.com>
2025-11-05 11:14:28 +0700

    first commit

---
+++ Diskon.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Diskon {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan total belanja: ");
+        int total = input.nextInt();
+        System.out.print("Apakah punya kartu member? (true/false): ");
+        boolean member = input.nextBoolean();
+
+        if (total >= 500000 || (member && total >= 300000)) {
+            System.out.println("Kamu dapat diskon spesial!");
+        } else {
+            System.out.println("Belum memenuhi syarat diskon.");
+        }
+    }
+}
